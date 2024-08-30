0704ec6ffade23d563f6add0915d61c2fe867003
giovanavi <giovana.gvi02@gmail.com>
2024-08-29 21:49:10 -0300

    att repository

--- src/main/java/com/vv/qxdconsulta/repository/HorarioDisponivelRepository.java
+++ src/main/java/com/vv/qxdconsulta/repository/HorarioDisponivelRepository.java
@@ -4,8 +4,12 @@ import com.vv.qxdconsulta.model.HorarioDisponivel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface HorarioDisponivelRepository extends JpaRepository<HorarioDisponivel, UUID> {
+
+    List<HorarioDisponivel> findByHorarioBetween(LocalDateTime dataComeco, LocalDateTime dataFinal);
 }
